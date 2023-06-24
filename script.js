dafc9e7e9f5545eda48bc6793b80e20794618483
Syed Rehan <40541331+rehan1124@users.noreply.github.com>
2023-06-24 20:18:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const MoodContractAddress = "0xf8983eb2f5d640ebe0ecde268d722c788c76bcde";
+const MoodContractABI = [
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+let MoodContract = "";
+let signer = "";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "sepolia");
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const mood = await MoodContract.getMood();
+  document.getElementById("showMood").innerText = `Your Mood: ${mood}`;
+  console.log(mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  await MoodContract.setMood(mood);
+}
